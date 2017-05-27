89204558d4cb9cfa1fa99fef64eb4bc83c679223
Alexandre Karpov <alexakarpov@gmail.com>
2017-05-27 17:14:31 -0400

    my tuple_to_list

---
+++ my_bifs.erl
@@ -0,0 +1,11 @@
+-module(my_bifs).
+-export([my_tuple_to_list/1]).
+
+my_tuple_to_list(T) ->
+    ttl_help(T, size(T), []).
+
+ttl_help(_, 0, Acc) -> Acc;
+ttl_help(T, N, Acc) -> ttl_help(T, N-1, [element(N, T)|Acc]).
+
+    
+   
