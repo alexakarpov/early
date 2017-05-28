e7dac5d5591a516480f83224153c2c40a35499b4
Alexandre Karpov <alexakarpov@gmail.com>
2017-05-27 22:21:04 -0400

    timing

--- my_bifs.erl
+++ my_bifs.erl
@@ -1,5 +1,5 @@
 -module(my_bifs).
--export([my_tuple_to_list/1]).
+-export([my_tuple_to_list/1, timing/1]).
 
 my_tuple_to_list(T) ->
     ttl_help(T, size(T), []).
@@ -7,5 +7,9 @@ my_tuple_to_list(T) ->
 ttl_help(_, 0, Acc) -> Acc;
 ttl_help(T, N, Acc) -> ttl_help(T, N-1, [element(N, T)|Acc]).
 
-    
-   
+
+timing(F) ->
+    X = erlang:system_time(),
+    F(),
+    Y = erlang:system_time(),
+    Y-X.
