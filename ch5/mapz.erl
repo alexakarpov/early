2ba9d921f273a0d31572133e99edd5896e862cea
Alex Karpov <alexakarpov@gmail.com>
2017-07-19 14:16:53 -0400

    back to it

--- ch5/mapz.erl
+++ ch5/mapz.erl
@@ -7,3 +7,13 @@ count_chars(Str) ->
 count_chars([H|T], X) ->
     count_chars(T, maps:update_with(H,fun(V) -> V+1 end, 1,X));
 count_chars([], X) -> X.
+
+%count_characters(Str) ->
+%    count_characters(Str, #{}).
+
+
+%count_characters([H|T], #{H := N} = X) ->
+%    count_characters(T, X#{H := N+1});
+%count_characters([H|T], X) ->
+%	count_characters(T, X#{H=>1});
+%count_characters([], X) -> X.
