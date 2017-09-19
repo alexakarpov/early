807b5622207cf71cb13cb8d1f270ef7c872d1b57
Alexandre Karpov <alexakarpov@gmail.com>
2017-09-18 21:50:28 -0400

    little rpc client/server

--- ch12/area_server.erl
+++ ch12/area_server.erl
@@ -1,12 +1,22 @@
 -module(area_server).
--export([loop/1]).
+-export([loop/0, rpc/2]).
 
 loop() ->
     receive
-        {rectangle, Width, Ht} ->
-            io:format("Rectangle area is ~p~n", [Width*Ht]),
+        {From, {rectangle, Width, Ht}} ->
+            From ! Width*Ht,
             loop();
-        {square, Side} ->
-            io:format("Square area is ~p~n", [Side*Side]),
+        {From, {square, Side}} ->
+            From ! Side*Side,
+            loop();
+        {From, Other} ->
+            From ! {error, Other},
             loop()
+    end.
+
+rpc(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+        Response ->
+            Response
     end.
