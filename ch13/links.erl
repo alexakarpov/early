11a5afdd82279d2f9129e3d5edba68abe0f0656e
Alexandre Karpov <alexakarpov@gmail.com>
2017-09-25 20:19:29 -0400

    end_of_chapter

--- ch13/links.erl
+++ ch13/links.erl
@@ -1,19 +1,43 @@
 -module(links).
--export([linkme/1, start/0, loop/1]).
+-export([start_many/1, loop_w/1, loop_m/1, on_exit/2]).
 
-linkme(Pid) ->
-    link(Pid).
-
-start() ->
-    spawn(links, loop, [[start]]).
-
-loop(History) ->
+loop_w(Name) ->
+    io:format("~p started on ~p~n", [Name, self()]),
     receive
-        halt -> exit(normal);
-        {command, {Sender, history}} ->
-            Sender ! History,
-            loop([]);
-        {msg, Message} ->
-            NewHistory = [Message | History],
-            loop(NewHistory)
+        halt ->
+            io:format("~p asked to stop.~n", [Name]),
+            exit(normal);
+        ping ->
+            io:format("pong~n"),
+            loop_w(Name);
+        {link, Pid} when is_pid(Pid) ->
+            link(Pid),
+            io:format("linked ~p to ~p~n", [self(), Pid]),
+            loop_w(Name)
         end.
+
+loop_m(Name) ->
+    receive
+        {'DOWN', _Ref, process, Pid, Why} ->
+            io:format("Process ~p is down with reason ~p~n",[Pid, Why]),
+            loop_m(Name)
+    end.
+
+on_exit(Pid, Fun) ->
+    spawn(fun() ->
+                  Ref = monitor(process, Pid),
+                  receive
+                      {'DOWN', Ref, process, Pid, Why} ->
+                          Fun(Why)
+                  end
+          end).
+
+start_many(Fs) ->
+    spawn(fun() ->
+                  [spawn_link(F) || F <- Fs],
+                  receive
+                      after
+                          infinity ->
+                              true
+                      end
+          end).
