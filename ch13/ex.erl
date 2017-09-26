81b8f3e448d693c8749d27ca5bd3f0ea18967d0c
Alexandre Karpov <alexakarpov@gmail.com>
2017-09-25 23:13:36 -0400

    ex3

--- ch13/ex.erl
+++ ch13/ex.erl
@@ -1,5 +1,5 @@
 -module(ex).
--export([my_spawn/3, foo/0]).
+-export([my_spawn/3, my_spawn/4, foo/0]).
 
 my_spawn(Mod, Func, Args) ->
     {_, _, StartTime} = erlang:timestamp(),
@@ -10,6 +10,24 @@ my_spawn(Mod, Func, Args) ->
             io:format("died because ~p after ~p~n", [Why, EndTime - StartTime])
     end.
 
+my_spawn(Mod, Func, Args, Time) ->
+    {Pid, Ref} = spawn_monitor(Mod, Func, Args),
+    receive
+        {'DOWN', Ref, process, Pid, Why} ->
+            io:format("died because ~p~n", [Why])
+    after
+        Time ->
+            io:format("I'm done~n"),
+            exit(Pid, timesup)
+    end.
+
 foo() ->
     io:format("foo~n"),
-    foo.
+    receive
+        _ ->
+             void
+    after
+        infinity ->
+            void
+    end.
+
