7cbcd9299f89316a37454827785b34055fa139bf
Alexandre Karpov <alexakarpov@gmail.com>
2017-09-27 19:31:10 -0400

    finished the monitor exercise. phew

--- ch13/ex.erl
+++ ch13/ex.erl
@@ -1,5 +1,11 @@
 -module(ex).
--export([my_spawn/3, my_spawn/4, foo/0]).
+-export([my_spawn/3,
+         my_spawn/4,
+         my_monitor/1,
+         loop_5_print/0,
+         monitor_listener/2,
+         solution4/1
+        ]).
 
 my_spawn(Mod, Func, Args) ->
     {_, _, StartTime} = erlang:timestamp(),
@@ -21,13 +27,42 @@ my_spawn(Mod, Func, Args, Time) ->
             exit(Pid, timesup)
     end.
 
-foo() ->
-    io:format("foo~n"),
+%% ============================ MONITORS work ==========================
+
+% simple "working" loop
+loop_5_print() ->
     receive
-        _ ->
-             void
-    after
-        infinity ->
-            void
+    after 5000 ->
+            io:format("I'm still alive~n"),
+            loop_5_print()
     end.
 
+
+solution4(Name) ->
+    Pid = spawn(ex, loop_5_print, []),
+    register(Name, Pid),
+    my_monitor(Name).
+
+
+% my monitor
+monitor_listener(Pid, Name) ->
+    monitor(process, Pid),
+    io:format("monitoring ~p at ~p~n", [Name, Pid]),
+    receive
+        {'DOWN', _Ref, process, Pid, Why} ->
+            io:format("~p (~p) died because ~p~n", [Pid, Name, Why]),
+            NewPid = restart(Name),
+            monitor_listener(NewPid, Name)
+    end.
+
+% recover action
+restart(Name) ->
+    io:format("Recovering ~p~n", [Name]),
+    Pid = spawn(ex,loop_5_print, []),
+    register(Name, Pid),
+    Pid.
+
+%function to bootstrapma monitor
+my_monitor(Name) ->
+    Pid = whereis(Name),
+    spawn(ex, monitor_listener, [Pid, Name]).
