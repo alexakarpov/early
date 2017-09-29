936cab96380b66e45c0b4e51a518c9035f987d7b
Alexandre Karpov <alexakarpov@gmail.com>
2017-09-29 15:00:21 -0400

    demonitoring

--- ch13/ex13.erl
+++ ch13/ex13.erl
@@ -16,31 +16,34 @@ worker(Name) ->
 
 orchestrate(Funs) ->
     Pids = [spawn(Fun) || Fun <- Funs],
-    MonitorPid = spawn(ex13finale, monitor_many, [Pids]),
+    MonitorPid = spawn(ex13, mon_bootstrap, [Pids]),
     io:format("Monitor started at ~p~n", [MonitorPid]).
     
 % bootstrap the multi-process monitor
 mon_bootstrap(Pids) ->
-    lists:foreach(fun(Pid) ->
-                          ARef = monitor(process, Pid),
-                          io:format("monitoring ~p with ~p~n", [Pid, ARef])
-                  end,
-                  Pids),
-    mon_listen(Pids).
+    Refs = lists:map(fun(Pid) ->
+                             monitor(process, Pid)
+                     end,
+                     Pids),
+    mon_listen(Pids, Refs).
 
-mon_listen(Pids) ->
+mon_listen(Pids, Refs) ->
     receive
         {'DOWN', _Ref, process, PidA, normal} ->
             io:format("detected normal exit of ~p~n", [PidA]),
-            mon_listen(Pids);
+            mon_listen(Pids, Refs);
         {'DOWN', Ref, process, PidB, Why} ->
             io:format("~p exited with ~p, giving all up~n",[PidB, Why]),
             lists:foreach(fun(Pid) ->
-                                  io:format("stopping ~p~n for cleanup~n", [Pid]),
+                                  io:format("stopping ~p for cleanup~n", [Pid]),
                                   exit(Pid, cleanup)
                           end,
-                         Pids),
-            mon_listen([])
+                          Pids),
+            lists:foreach(fun(ARef) ->
+                                  demonitor(ARef)
+                          end,
+                          Refs),
+            mon_listen([],[])
     end.
 
 
