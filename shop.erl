2cd9b61c29e34fd7745b4b1cbb1c1b3b50a01d8c
Alexandre Karpov <akarpov@Alexandres-MacBook-Air.local>
2017-09-29 20:26:48 -0400

    cleanup

--- shop.erl
+++
@@ -1,9 +0,0 @@
--module(shop).
--export([cost/1]).
-
-cost(orange) -> 5;
-cost(newspaper) ->8;
-cost(apples) -> 2;
-cost(pears) -> 9;
-cost(milk) -> 7;
-cost(_) -> 0. % if we don't carry it, it's free!!
--- shop1.erl
+++
@@ -1,6 +0,0 @@
--module(shop1).
--export([total/1]).
-
-total([])->
-    0;
-total([{What, N}|T]) -> cost:cost(What) * N + total(T).
