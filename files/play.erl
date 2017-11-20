a3c4c40efef89d70be7ef4a81fa683d602431ca1
Alexandre Karpov <alexakarpov@gmail.com>
2017-11-20 10:09:54 -0500

    should_recompile knocked off

--- files/play.erl
+++ files/play.erl
@@ -1,5 +1,5 @@
 -module(play).
--export([unconsult/2, file_size_and_type/1, ls/1]).
+-export([unconsult/2, file_size_and_type/1, should_recompile/1, ls/1]).
 -include_lib("kernel/include/file.hrl").
 
 unconsult(File, L) ->
@@ -19,3 +19,18 @@ ls(Dir) ->
     {ok, L} = file:list_dir(Dir),
     lists:map(fun(I) ->
                        {I, file_size_and_type(I)} end, lists:sort(L)).
+
+should_recompile(Mod) ->
+    case file:read_file_info(Mod++".erl") of
+        {ok, Info} ->
+            Mtime = Info#file_info.mtime,
+            io:format("~p~n", [Mtime]),
+            case file:read_file_info(Mod++".beam") of
+                {error, enoent} ->
+                    true;
+                {ok, BeamInfo} ->
+                    BeamInfo#file_info.mtime < Mtime
+            end;
+        {error, Why} ->
+            {error, Why}
+    end.
