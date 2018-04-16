335083e338790ede1a4cb1a3cc5ec14de78d45ef
Alexandre Karpov <alexakarpov@gmail.com>
2018-04-16 17:10:15 -0400

    done with par_server, registration is freaking hard

--- ch17-sockets/tcp2.erl
+++ ch17-sockets/tcp2.erl
@@ -3,33 +3,38 @@
 -define(Port, 2345).
 
 loop(Socket) ->
+    report("About to enter the [blocking] receive"),
     receive
         {tcp, Socket, Bin} ->
-            io:format("Server received binary = ~p~n", [Bin]),
+            report({msg, "Server received binary",
+                    bin, Bin}),
             Str = binary_to_term(Bin),
-            io:format("Server (unpacked) ~p~n", [Str]),
+            report({"unpacked ", Str}),
             Reply = lib_misc:string2value(Str),
-            io:format("Server replying = ~p~n", [Reply]),
+            io:format("Server replying with ~p to ~p~n", [Reply, Socket]),
             gen_tcp:send(Socket, term_to_binary(Reply)),
+            inet:setopts(Socket, [{active, once}]),
+            io:format("Looping~n",[]),
             loop(Socket);
         {tcp_closed, Socket} ->
-            io:format("Server connection closed~n")
+            io:format("server at ~p is about to exit...~n", [ self()])
     end.
 
+report(Msg) ->
+    io:format("~p reporting: ~p~n", [self(), Msg]).
+
 par_connect(Listen) ->
-    io:format("~p is accepting connections~n", [self()]),
+    report("in par_connect, accepting (blocking)..."),
     {ok, Socket} = gen_tcp:accept(Listen),
-    io:format("~p accepted a new connection~n",[self()]),
-    io:format("Socket options: ~p~n", [inet:getopts(Socket, [socket_getopts])]),
-    spawn(fun() ->
-                  par_connect(Listen) end),
+    report("accepted a new connection"),
+    Pid = spawn(fun() -> par_connect(Listen) end),
+    io:format("newly spawed ~p before entering loop(..)~n", [Pid]),
     loop(Socket).
 
 start_parallel_server() ->
     {ok, Listen} = gen_tcp:listen(?Port, [binary, {packet, 4},
                                           {reuseaddr, true},
-                                          {active, true}]),
+                                          {active, once}]),
     io:format("Listen: ~p~n", [Listen]),
     Pid1 = spawn(fun() -> par_connect(Listen) end),
-    register(par_server, self()),
-    io:format("Originally spawned acceptor ~p~n", [Pid1]).
+    io:format("shell (~p) spawned the first acceptor ~p~n", [self(), Pid1]).
