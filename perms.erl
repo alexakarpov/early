2e75249673d8b9e3109cd4ecccc4c8f583bbd553
Alexandre Karpov <alexakarpov@gmail.com>
2018-05-09 17:42:31 -0400

    powersets and 237 solution, ineffective

--- perms.erl
+++ perms.erl
@@ -1,14 +1,32 @@
 -module(perms).
--export([checkDivisibility/1]).
+-export([sums/1, count_sets/1, limited_powerset/2]).
 
-permutations([]) -> [[]];
-permutations(L) -> [[H|T] || H <- L, T <- permutations(L--[H])].
 
-checkDivisibility(Arr) -> lists:map(
-              fun(S) ->
-                      Perms = permutations(S),
-                      Nums = lists:map(fun(L) -> list_to_integer(L) end, Perms),
-                      Divisibles = lists:filter(fun(N) -> N rem 8 == 0 end, Nums),
-                      if Divisibles == [] -> "NO"; true -> "YES" end
-              end,
-              Arr).
+
+limited_powerset(L, N) ->
+    {_, Acc} = generate(L, N),
+    Acc.
+
+generate([], _) ->
+    {[[]], []};
+generate([H|T], N) ->        
+    {PT, Acc} = generate(T, N),
+    generate(H, PT, PT, Acc, N).
+
+generate(_, [], PT, Acc, _) ->
+    %io:format("g5 base returns PT:~p,~n Acc:~p}~n",[PT, Acc]),
+    {PT, Acc};
+generate(X, [H|T], PT, Acc, N) when length(H)=/=N-1 ->
+    generate(X, T, [[X|H]|PT], Acc, N);
+generate(X, [H|T], PT, Acc, N) ->    
+    generate(X, T, [[X|H]|PT], [[X|H]|Acc], N).
+
+sums([]) -> 0;
+sums([H|T]) ->
+    H + sums(T).
+
+count_sets(L) ->
+    S = limited_powerset(L, 237),
+    length(S).
+
+    
