4d0ef4287ead3e1ba9a5a650d77eda0cdc338a20
Alexandre Karpov <alexakarpov@gmail.com>
2018-05-09 22:04:14 -0400

    ct test conquered

--- codefights/codefights_SUITE.erl
+++ codefights/codefights_SUITE.erl
@@ -1,16 +1,29 @@
 -module(codefights_SUITE).
-
--include_lib("common_test/include/ct.hrl").
-
--export([all/0]).
+-compile(export_all).
 
 all() ->
-    [testFoo,
-     testBar].
+    [testCrypt1,
+     testCrypt2,
+     testCrypt3,
+     testCrypt4].
 
-testFoo(Config) ->
-    {ok, N} = codefights:foo(1).
+testCrypt1(_Config) ->
+    true == codefights:isCryptSolution(["AA", "AB", "BC"],
+                                         [["A","1"],
+                                          ["B","2"],
+                                          ["C","3"]]).
 
-testBar(Config) ->
-    {nope, X} = codefights:foo(2).
-
+testCrypt2(_Config) ->
+    true == codefights:isCryptSolution(["A", "BA", "BA"],
+                                       [["A","0"],
+                                        ["B","2"]]).
+testCrypt3(_Config) ->
+    false == codefights:isCryptSolution(["AB", "AB", "AC"],
+                                       [["A","0"],
+                                        ["B","2"],
+                                        ["C","4"]]).
+testCrypt4(_Config) ->
+    false == codefights:isCryptSolution(["AA", "BB", "CC"],
+                                       [["A","1"],
+                                        ["B","2"],
+                                        ["C","4"]]).
